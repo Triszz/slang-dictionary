0ebe0516226635ea13d222b272a5686b3c2b1c7a
Triszz <tranthanhtri23022005@gmail.com>
2025-11-06 12:07:19 +0700

    Add functions in SlangDictionary: addSlangWord, deleteSlangWord, getRandomSlangWord, resetDictionary, saveDictionary, loadDictionary, saveHistory, loadHistory, getTotalWords, getDictionary.

--- src/SlangDictionary.java
+++ src/SlangDictionary.java
@@ -4,10 +4,10 @@ import java.util.stream.Collectors;
 
 public class SlangDictionary {
     private HashMap<String, List<String>> dictionary;
-    private HashMap<String, List<String>> originalDictionary;
+    private final HashMap<String, List<String>> originalDictionary;
     private ArrayList<String> searchHistory;
-    private String data_file = "dictionary.dat";
-    private String history_file = "history.dat";
+    private static final String data_file = "dictionary.dat";
+    private static final String history_file = "history.dat";
 
     public SlangDictionary() {
         this.dictionary = new HashMap<>();
@@ -15,7 +15,9 @@ public class SlangDictionary {
         this.searchHistory = new ArrayList<>();
     }
 
-    // Load slang words from file
+    /**
+     * Load slang words from file
+     */
     public void loadFromFile(String filePath) {
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
@@ -46,28 +48,38 @@ public class SlangDictionary {
         }
     }
 
-    // Search by slang word
+    /**
+     * Search by slang word
+     */
     public List<String> searchBySlangWord(String word) {
-        addToHistory("Slang: " + word);
+        addToHistory("Slang word: " + word);
         return dictionary.getOrDefault(word, null);
     }
 
-    // Add search history
+    /**
+     * Add search history
+     */
     public void addToHistory(String word) {
         searchHistory.add(word);
     }
 
-    // Get search history
+    /**
+     * Get search history
+     */
     public List<String> getSearchHistory() {
         return new ArrayList<>(searchHistory);
     }
 
-    // Clear search history
+    /**
+     * Clear search history
+     */
     public void clearHistory() {
         searchHistory.clear();
     }
 
-    // Search by definition
+    /**
+     * Search by definition
+     */
     public List<String> searchByDefinition(String keyword) {
         addToHistory("Definition: " + keyword);
         return dictionary.entrySet().stream()
@@ -76,4 +88,109 @@ public class SlangDictionary {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Add new slang word
+     */
+    public boolean addSlangWord(String word, String definition) {
+        if(dictionary.containsKey(word))
+            return false;
+        List<String> defList = new ArrayList<>();
+        defList.add(definition);
+        dictionary.put(word, defList);
+        return true;
+    }
+
+    /* TODO: Edit slang word */
+
+    /**
+     * Delete slang word
+     */
+    public boolean deleteSlangWord(String word) {
+        return dictionary.remove(word) != null;
+    }
+
+    /**
+     * Get random slang word
+     */
+    public String getRandomSlangWord() {
+        if(dictionary.isEmpty())
+            return null;
+
+        List<String> words = new ArrayList<>(dictionary.keySet());
+        Random random = new Random();
+        return words.get(random.nextInt(words.size()));
+    }
+
+    /**
+     * Reset to original dictionary
+     */
+    public void resetDictionary() {
+        dictionary.clear();
+        dictionary.putAll(originalDictionary);
+        System.out.println("Dictionary reset to original!");
+    }
+
+    /**
+     * Save dictionary to file
+     */
+    public void saveDictionary() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(data_file))) {
+            oos.writeObject(dictionary);
+            System.out.println("Dictionary save to file!");
+        } catch (IOException e) {
+            System.out.println("Error saving dictionary: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Load dictionary from file
+     */
+    public boolean  loadDictionary() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(data_file))) {
+            dictionary = (HashMap<String, List<String>>) ois.readObject();
+            System.out.println("Dictionary loaded from cached file!");
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println("No cached dictionary found. Loading from original file...");
+            return false;
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error loading dictionary: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Save history to file
+     */
+    public void saveHistory() {
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(history_file))) {
+            oos.writeObject(searchHistory);
+            System.out.println("History save to file!");
+        } catch (IOException e) {
+            System.out.println("Error saving history: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Load history from file
+     */
+    public boolean loadHistory() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(history_file))) {
+            searchHistory = (ArrayList<String>) ois.readObject();
+            return true;
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error loading dictionary: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // Getters
+    public int getTotalWords() {
+        return dictionary.size();
+    }
+
+    public HashMap<String, List<String>> getDictionary() {
+        return dictionary;
+    }
 }
+
